b4a94003eae14c5bdacedcfccd47fbf84d054170
Veeresh <imveeresh6454@gmail.com>
2024-12-02 18:48:28 +0530

    Appium Demo Using Options class

---
+++ AppiumDemo/src/test/java/CreateDriverSessionUsingOptions.java
@@ -0,0 +1,27 @@
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.WebElement;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class CreateDriverSessionUsingOptions
+{
+    public static void main(String[] args) throws URISyntaxException, MalformedURLException {
+        UiAutomator2Options options = new UiAutomator2Options().
+                setDeviceName("pixel_8").
+                setAutomationName("UiAutomator2").
+                setPlatformName("Android").
+                setApp("C:/Users/My Laptop/OneDrive/Desktop/Veeresh/Appium/Android App/ApiDemos-debug.apk");
+
+        URL url = new URI("http://127.0.0.1:4723/").toURL();
+
+        AppiumDriver driver = new AppiumDriver(url, options);
+
+        WebElement ele = driver.findElement(AppiumBy.accessibilityId("Access'ibility"));
+        System.out.print(ele.getText());
+    }
+}
